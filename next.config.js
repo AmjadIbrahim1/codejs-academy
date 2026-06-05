390094e2d38aeb7fac5f755c82229b27ca1cea62
Amjad <amjadibrahim218@gmail.com>
2026-06-05 13:48:19 +0300

    ok

--- next.config.js
+++ next.config.js
@@ -1,10 +1,40 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  compress: true,
+
+  reactStrictMode: true,
+  poweredByHeader: false,
+
+  experimental: {
+    optimizePackageImports: [
+      "@trpc/react-query",
+      "@trpc/client",
+    ],
+  },
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 1080, 1920],
+  },
+
+  // 🔥 أهم سطر في الحل
+  output: "standalone",
+
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: [
+        "**/node_modules/**",
+        "C:/Users/**",
+        "**/Application Data/**",
+        "**/AppData/**",
+        "C:\\Users\\Elamen Soft\\**",
+      ],
+    };
+
+    return config;
+  },
+};
 
 export default config;
